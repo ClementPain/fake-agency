68923457cc105fe30e198c87fb7d1246b7016c86
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-10-28 16:44:20 +0100

    adding CaseStudy Component

---
+++ src/data/studies.js
@@ -0,0 +1,28 @@
+const studies = [
+    {
+        clientname: "platon",
+        title: "PLATON",
+        subtitleFr:"Le challenge",
+        subtitleEn:"The challenge",
+        contentFr: "PLATON a décidé de se lancer à l'époque malgré une crise économique difficile. Nous avons été derrière eux pour leur apporter le meilleur du Web et du digital. Nous sommes vraiment la meilleure agence.",
+        contentEn: "PLATON decided to get started at the time despite a difficult economic crisis. We have been behind them to bring them the best of the Web and digital. We are truly the best agency."
+    },
+    {
+        clientname: "solane",
+        title: "Solane",
+        subtitleFr: "Solane est le premier vendeur de fraises du Poitou-Charentes",
+        subtitleEn: "Solane is the leading strawberry seller in Poitou-Charentes",
+        contentFr: "Et c'est grâce à nous. Dans les moments les meilleurs comme les plus durs, nos campagnes de communication sur la place du marché ont fonctionné.",
+        contentEn: "And it's thanks to us. In both the best and the hardest times, our marketplace communication campaigns worked."
+    },
+    {
+        clientname: "sedal",
+        title: "Sedal",
+        subtitleFr: "Sedal, l'entreprise qui a commencé tout en bas. Et qui est toujours tout en bas",
+        subtitleEn: "Sedal, Started from the bottom. Still on the bottom",
+        contentFr: "Malgré nos conseils et notre site web créé sur Wix, Sedal reste semble ne pas vouloir décoller. Mais comme un grand homme et une grande femme l'ont dit, \"les derniers seront les premiers\".",
+        contentEn:"Despite our advice and our website created on Wix, Sedal remains does not want to take off. But as a great man and a great woman said, \"the last will be the first\"."
+    }
+]
+
+export default studies;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl' ;
+import { Link } from 'react-router-dom';
+import CaseStudy from '../components/CaseStudy';
 
-const Work = () => (
+const Work = () => {
+  const [projects, setProjects] = useState([]);
+
+  return (
   <div className="container">
     <div className="row">
       <h1><FormattedMessage id='work.title' /></h1>
@@ -9,7 +14,11 @@ const Work = () => (
     <div className="row">
       <p><FormattedMessage id='work.description' /></p>
     </div>
+    <div className="row">
+      <CaseStudy />
+    </div>
   </div>
-);
+  );
+};
 
 export default Work;
